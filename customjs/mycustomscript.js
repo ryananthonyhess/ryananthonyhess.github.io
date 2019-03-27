9858c210148cdef3051b03183193256c4c14ae33
Ryan Anthony Hess <37939083+ryananthonyhess@users.noreply.github.com>
2019-03-27 10:25:36 -0400

    update

--- customjs/mycustomscript.js
+++ customjs/mycustomscript.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
         alert('Unfortunately this feature is currently down for maintenance and will return better than ever shortly. We apologize for any inconvenience this may cause and wish you a wonderful day!')
     });
 });
+
+//for the email
+$(document).ready(function(){
+    $('.buttonemail').on('click',function(){
+       window.location.href = "mailto:address@dmail.com"; 
+    });
+});
