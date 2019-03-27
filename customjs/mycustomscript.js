8dcd306fb6c2d8090fb347fb54985dbbca2872d7
Ryan Anthony Hess <37939083+ryananthonyhess@users.noreply.github.com>
2019-03-27 09:13:41 -0400

    initial commit

---
+++ customjs/mycustomscript.js
@@ -0,0 +1,6 @@
+//Creates an alert on button click 
+$(document).ready(function() {
+    $('#buttonevent').on('click', function () {
+        alert('Unfortunately this feature is currently down for maintenance and will return better than ever shortly. We apologize for any inconvenience this may cause and wish you a wonderful day!')
+    });
+});
